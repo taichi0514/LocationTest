61d961b75fdd7038ba9a9bb64bc629045422258c
taichimurai <taichimurai0514@gmail.com>
2020-02-05 12:00:24 +0900

    Initial Commit

---
+++ Location test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Location test
+//
+//  Created by taichimurai on 2020/02/05.
+//  Copyright © 2020 taichimurai. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Location testTests/Location_testTests.swift
@@ -0,0 +1,34 @@
+//
+//  Location_testTests.swift
+//  Location testTests
+//
+//  Created by taichimurai on 2020/02/05.
+//  Copyright © 2020 taichimurai. All rights reserved.
+//
+
+import XCTest
+@testable import Location_test
+
+class Location_testTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
